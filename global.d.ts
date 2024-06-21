24876637ec096cb06aebb9b21f1de2c401161c73
deivoff <deivoff@gmail.com>
2024-06-21 15:34:15 +0500

    fix svg build

--- global.d.ts
+++ global.d.ts
@@ -6,3 +6,10 @@ declare global {
   // Use type safe message keys with `next-intl`
   interface IntlMessages extends Messages {}
 }
+
+declare module '*.svg' {
+  import { FC, SVGProps } from 'react';
+
+  const content: FC<SVGProps<SVGElement>>;
+  export default content;
+}
